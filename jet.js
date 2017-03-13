8f4a40b10097cb98314baeb3b5ebfb85bd60d0ac
yanivsalzberg1 <yanivsalzberg@gmail.com>
2017-03-13 11:18:04 +0200

    fix merge conflict

---
+++ jet.js
@@ -0,0 +1,15 @@
+var Jet = function (color, size) {
+  this.color = color;
+  this.size = size;
+
+<<<<<<< HEAD
+  this.fly = function () {
+    console.log('whoosh!');
+=======
+  this.takeOff = function () {
+    console.log('lift off!');
+>>>>>>> master
+  }
+}
+
+module.exports = Jet;
